5657ab60ad743efc5cfedcacaed20e33d1c027b5
dayens13 <dionsunardi1999@gmail.com>
2021-12-03 14:24:23 +0700

    Menambahkan ListView Builder

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dion_pemula1/model/hardware_data.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -6,6 +8,61 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Hardware Computer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.account_circle_outlined),
+            tooltip: 'Profile',
+            onPressed: () {
+
+            },
+          ),
+        ],
+      ),
+
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final HardwareData hardware = hardwareDataList[index];
+          return InkWell(
+            onTap: () {
+
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(hardware.image),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            hardware.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            hardware.description,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: hardwareDataList.length,
       ),
     );
   }
---
+++ lib/model/hardware_data.dart
@@ -0,0 +1,49 @@
+class HardwareData {
+  String image;
+  String name;
+  String description;
+
+  HardwareData({
+    required this.image,
+    required this.name,
+    required this.description
+});
+}
+
+var hardwareDataList = [
+  HardwareData(
+    image: 'images/processor.png',
+    name: 'Processor',
+    description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  ),
+  HardwareData(
+      image: 'images/processor.png',
+      name: 'Processor',
+      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+  )
+];
