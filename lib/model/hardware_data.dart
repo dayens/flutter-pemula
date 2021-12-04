9b8047fde995a2e7434262c984baf49dbc5f380f
dayens13 <dionsunardi1999@gmail.com>
2021-12-04 14:07:15 +0700

    Finish Profile Screen

--- lib/model/hardware_data.dart
+++ lib/model/hardware_data.dart
@@ -18,18 +18,18 @@ var hardwareDataList = [
   ),
   HardwareData(
       image: 'images/processor.png',
-      name: 'Processor',
-      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+      name: 'MainBoard',
+      description: 'Merupakan papan sirkuit atau papan induk yang berfungsi sebagai tempat terpasangnya komponen-komponen computer.'
   ),
   HardwareData(
       image: 'images/processor.png',
-      name: 'Processor',
-      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+      name: 'RAM',
+      description: 'RAM atau Random Acces Memory merupakan perangkat keras yang memiliki fungsi untuk menyimpan  data serta-intruksi yang dilakukan/diberikan oleh prosessor.'
   ),
   HardwareData(
       image: 'images/processor.png',
-      name: 'Processor',
-      description: 'Merupakan sebuah IC yang memiliki fungsi untuk mengontrol semua proses atau jalannya sebuah sistem komputer.  processor bertugas melakukan perhitungan aritmatika dan logika pada sebuah komputer serta memberikan instruksi untuk menjalankan program yang akan dijalankan oleh komputer yang diinput oleh user (branware). Processor juga dapat diartikan sebagai otak dari sebuah system computer.'
+      name: 'Fan & Heatsink',
+      description: 'Memiliki fungsi untuk mengurangi panas yang dihasilkan oleh computer computer, Fan & Headsink juga memiliki fungsi sebagai pendingin processor agar tidak overhead.'
   ),
   HardwareData(
       image: 'images/processor.png',
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -23,6 +23,43 @@ class ProfileScreen extends StatelessWidget {
                   height: 240,
                 ),
               ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 15, left: 8, right: 8),
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2)
+            ),
+            child: Center(
+              child: Text(
+                'Nama  : Dion Sunardi\nEmail   : dionsuanrdi1999@gmail.com',
+                style: TextStyle(fontSize: 16.0),),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 64, left: 8, right: 8),
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2)
+            ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset('images/instagram.png',width: 32,height: 32),
+                    Text('   Dion Sunardi')
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('dayens   '),
+                    Image.asset('images/github.png', width: 26, height: 26,),
+                  ],
+                )
+              ],
             ),
           )
         ],
