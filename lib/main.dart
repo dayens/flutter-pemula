9bbfa57a3a373a3dc214355c7b280cd82210fd27
dayens13 <dionsunardi1999@gmail.com>
2021-12-05 07:54:31 +0700

    Fix Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dion_pemula1/splash.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter_dion_pemula1/home_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -9,7 +11,21 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       title: 'Hardware Computer',
       theme: ThemeData(),
-      home: Splash(),
+      home: AnimatedSplashScreen(splash:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('images/icon_comp.png', width: 48, height: 48),
+              Text(
+                'Hardware Computer',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+              )
+            ],
+          ),
+          //Image.asset('images/icon_comp.png'),
+          duration: 3000,
+          splashTransition: SplashTransition.scaleTransition,
+          nextScreen: HomeScreen()),
     );
   }
 }
